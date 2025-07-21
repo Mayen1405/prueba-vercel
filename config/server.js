22054e037ff12913bc324d70b310a2521022492d
Mayen1405 <melgar14jose@gmail.com>
2025-07-20 18:26:50 -0600

    fix

--- config/server.js
+++ config/server.js
@@ -1,46 +1,46 @@
-import express from 'express';
-import helmet from 'helmet';
-import morgan from 'morgan';
-import apiLimiter from '../src/middlewares/rate-limit-validator.js';
-import cors from 'cors';
-import productRoutes from '../src/product/product.routes.js';
-import authRoutes from '../src/auth/auth.routes.js';
-import userRoutes from '../src/user/user.routes.js';
-import accountRoutes from '../src/account/account.routes.js';
-import transactionRoutes from '../src/transactions/transaction.routes.js';
-import currencyRoutes from '../src/currency/currency.routes.js';
-import { dbConnection } from './mongo.js';
-import defaultData from './default-data.js'; 
-import { createDefaultProducts } from './default-products.js';
-import { createDefaultAccounts } from './default-accounts.js';
+import { dbConnection } from "./mongo.js";
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import apiLimiter from "../src/middlewares/rate-limit-validator.js";
+import productRoutes from "../src/product/product.routes.js";
+import authRoutes from "../src/auth/auth.routes.js";
+import userRoutes from "../src/user/user.routes.js";
+import accountRoutes from "../src/account/account.routes.js";
+import transactionRoutes from "../src/transactions/transaction.routes.js";
+import defaultData from "./default-data.js";
+import { createDefaultProducts } from "./default-products.js"; 
+import { createDefaultAccounts } from "./default-accounts.js"; 
+import currencyRoutes from "../src/currency/currency.routes.js"; 
 
 const middlewares = (app) => {
+    app.use(express.urlencoded({ extended: false }));
     app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
     app.use(cors({
-    origin: [
-      'https://prueba-vercel-front.firebaseapp.com'
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH','OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true   
-}));
+        origin: [
+            'http://localhost:5173',
+            'https://prueba-vercel-front.web.app',
+            'https://prueba-vercel-front.firebaseapp.com'
+        ],
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH','OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: true   
+    }));
+
     app.use(helmet());
     app.use(morgan("dev"));
     app.use(apiLimiter);
 };
 
 const routes = (app) => {
-    app.get('/', (req, res) => {
-        res.send('API CCI funcionando!');
-    });
     app.use("/cci/v1/product", productRoutes);
-    app.use("/cci/v1/auth", authRoutes);
+    app.use("/cci/v1/auth", authRoutes)
     app.use("/cci/v1/user", userRoutes);
     app.use("/cci/v1/account", accountRoutes);
     app.use("/cci/v1/transaction", transactionRoutes);
     app.use("/cci/v1/currency", currencyRoutes);
-};
+}
 
 const connectDB = async () => {
     try {
@@ -50,19 +50,21 @@ const connectDB = async () => {
         await createDefaultAccounts();
     } catch (error) {
         console.error("Database connection failed:", error);
-        throw error;
+        process.exit(1); 
     }
-};
+}
 
-export const buildApp = async () => {
+export const initServer = () => {
     const app = express();
-    middlewares(app);
     try {
-        await connectDB();
-    } catch (error) {
-        console.error("Error al conectar a la base de datos:", error);
-        // Continuamos con la aplicación incluso si falla la conexión
+        middlewares(app);
+        connectDB();
+        routes(app);
+        const port = process.env.PORT;
+        app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+        });
+    } catch (err) {
+        console.log(`Server init failed: ${err}`);
     }
-    routes(app);
-    return app;
 };
